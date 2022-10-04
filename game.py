81d9050931a61af945b80ab1472d922ac7c11965
Jonas Camillus Jeppesen <jonascj@jonascj.dk>
2022-10-04 13:10:11 +0200

    Added basic pacman and keyboard controls

--- game.py
+++ game.py
@@ -1,21 +1,43 @@
 # Pac-Man clone made for learning/teaching
 import time
+import random
 import pygame as pg
 
+## Setup ##
 pg.init()
-
 screen = pg.display.set_mode((300,400))
+pg.display.set_caption("Pac-Man (clone)")
+
+x = 300/2 
+y = 400/2 
 
 running = True
 while running:
+
+    # Event loop
     events = pg.event.get()
     for event in events:
         if event.type == pg.QUIT:
             running = False
 
+        elif event.type == pg.KEYDOWN:
+            if event.key == pg.K_LEFT:
+                x = x-5
+            elif event.key == pg.K_RIGHT:
+                x = x+5
+            elif event.key == pg.K_UP:
+                y = y-5
+            elif event.key == pg.K_DOWN:
+                y = y+5
+
+    # Draw
     screen.fill((0,0,0))
 
+    pg.draw.circle(screen, (220,220,0), (x, y), 20)
+            
+
+    # Update screen
     pg.display.flip()
 
+    # Framerate (limit by doing nothing)
     time.sleep(0.05)
-
